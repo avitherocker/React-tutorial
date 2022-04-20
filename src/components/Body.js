c31e0cf0b8c0fa5579817fb75ed1edaae3b85af3
abhilash <pandeyabhilash809@gmail.com>
2022-04-20 17:14:53 +0530

    Form in react

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,56 @@
 import React from "react";
 
+class Body extends React.Component {
+    constructor(props){
+           super(props);
+           this.state={
+               name:"",
+               class:""
+           }
 
-class Body extends React.Component{
-
-             
-
-
-
-              render(){
-
-
-                return (<div>Texting</div>)
-              }
+           this.handleChange=this.handleChange.bind(this);
+    }
 
 
+    submitForm(e){
+
+        e.preventDefault();
+        
+
+
+    }
+
+
+    handleChange(e){
+
+        this.setState({
+
+             [e.target.name]:e.target.value
+
+        })
+    }
+
+  render() {
+    return (
+
+        //Controllable Component
+         <React.Fragment>
+            
+                <form  onSubmit={this.submitForm}>
+
+                   <input name="name" value={this.state.name}  onChange={this.handleChange} ></input>
+                   <input name="class" value={this.state.class}  onChange={this.handleChange} ></input>
+
+                </form>
+
+
+
+
+         </React.Fragment>
+
+
+    );
+  }
 }
-
 
 export default Body;
